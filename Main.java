9e0fa502dd3fa10b95ea4f7aa52dfb7a86cc0c1e
ellis  velandia caicedo <100318892+Ellisvelandia@users.noreply.github.com>
2023-04-04 23:44:12 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+class Main {
+  
+  public static void main(String[] arguments) {
+
+    // integer types
+    byte aSingleByte = 100;
+    short aSmallNumber = 20000;
+    int anInteger = 2147483647;
+    long aLargeNumber = 922337203684775807L;
+
+   // decimal Types
+    double aDouble = 1.54899;
+    float aFloat = 3.4028F;   
+
+    // booleans
+   boolean isWeekend = false;
+   boolean isWorkDay = true;
+
+    // characters
+    char copyrightSymbol = '\u00A9';
+    
+    // int age;
+    // age = 27;
+    System.out.println("This is a the copyright symbol" + " " + copyrightSymbol + " years old.");
+  }
+}
+
+
+
